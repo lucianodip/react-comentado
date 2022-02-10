84de3b92a71a5af4678956243efb042e51e81cb3
unknown <dipasquale.luc@gmail.com>
2022-02-10 16:57:51 -0300

    nav

--- proyecto-coder/src/components/NavBar/NavBar.js
+++ proyecto-coder/src/components/NavBar/NavBar.js
@@ -4,9 +4,42 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import {Link} from 'react-router-dom'
 
+ 
+
 export const NavBar = () => {
     return (
-        <header className='header'>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link to='/'>
+                        <img src='./logofondo.jpg' width='50' />
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mx-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Categorias</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Ofertas</Link>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+
+
+{/* <header className='header'>
             <Link to='/' style={{color: '#FFF'}}>
                 <h1>LOGO</h1>
             </Link>
@@ -17,6 +50,4 @@ export const NavBar = () => {
                 <Link to='/productos/gorros' className='header-link'>Gorros</Link>
             </nav>
             
-        </header>
-    )
-}
+        </header> */}
